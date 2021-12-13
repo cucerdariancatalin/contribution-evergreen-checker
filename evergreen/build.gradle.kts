2b435cc8f902f46077eca237eed157301b5242fc
Manas Tungare <manas@google.com>
2021-12-13 14:00:10 -0800

    Remove unused import `org.jetbrains.kotlin.gradle.tasks.KotlinCompile`

--- evergreen/build.gradle.kts
+++ evergreen/build.gradle.kts
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
   id("com.android.application")
   id("kotlin-android")
